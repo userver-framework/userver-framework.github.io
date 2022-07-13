1cc2357bc2808dfcf16fcb633e6789145b3421a4
Antony Polukhin <antoshkka@gmail.com>
2022-07-13 15:07:06 +0300

    Docs regenerated

--- search/pages_3.js
+++ search/pages_3.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['debian_2d11x32_0',['debian-11x32',['../d6/d79/md_en_deps_debian_11x32.html',1,'']]],
-  ['dynamic_20configs_1',['Dynamic configs',['../dd/d2c/md_en_schemas_dynamic_configs.html',1,'']]]
+  ['driver_20writing_20guide_1',['Driver Writing Guide',['../d6/d18/md_en_userver_driver_guide.html',1,'']]],
+  ['dynamic_20configs_2',['Dynamic configs',['../dd/d2c/md_en_schemas_dynamic_configs.html',1,'']]]
 ];
